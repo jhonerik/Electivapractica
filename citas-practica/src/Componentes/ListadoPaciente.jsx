7e501f27768778e65e9e564ddc9ab5bc97e70e17
jhonerik <j-bermudezb@corhuila.edu.co>
2023-01-23 20:22:36 -0500

    nuevosavecoming

--- citas-practica/src/Componentes/ListadoPaciente.jsx
+++ citas-practica/src/Componentes/ListadoPaciente.jsx
@@ -1,23 +1,37 @@
 import Pacientes from "./Pacientes"
 
 
-function ListadoPaciente({pacientes}) {
+function ListadoPaciente({pacientes, setPaciente}) {
+  //formulas para id unico
+  //Date.now().toString(36) 
+  //Math.random().toString(36).substr(2)
   return (
    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+    {pacientes && pacientes.length ? 
+    <>
       <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
       <p className="text-xl mt-5 mb-10 text-center">
         Administra tus {' '}
         <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
       </p>
-
-      {pacientes.map((indice)=>
+    
+      {pacientes.map(indice=>
         <Pacientes 
+          key = {indice.id}
           indice = {indice}
+          setPaciente = {setPaciente}
         />
       )}
 
-      
-      
+    </>:
+    <>
+      <h2 className="font-black text-3xl text-center">No hay pacientes!</h2>
+      <p className="text-xl mt-5 mb-10 text-center">
+        Comienza agregando pacientes {' '}
+        <span className="text-indigo-600 font-bold">y aparecerán en este espacio!</span>
+      </p>
+    </>
+    }
    </div>
   )
 }
